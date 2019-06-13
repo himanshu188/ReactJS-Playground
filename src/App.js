b90580572f5f5aae1c8e529ee343100fb2352f5b
Himanshu Patel <ptlhimanshu827@gmail.com>
2019-06-13 10:21:57 -0400

    Enabled CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot} from  'radium';
 class App extends Component{
 
   state = {
@@ -47,10 +46,6 @@ class App extends Component{
       border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer',
-      ':hover' : {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
     };
 
     let persons = null;
@@ -70,33 +65,29 @@ class App extends Component{
         </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
-    const classes = [];
+    const assignedClasses = [];
 
     if(this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if(this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-    <StyleRoot>
-        <div className="App">
+
+        <div className={classes.App}>
           <h1>Hi, This is My React Playground</h1>
-          <p className={classes.join(' ')}> That's working great!</p>
+          <p className={assignedClasses.join(' ')}> That's working great!</p>
           <button
             style={style}
             onClick={() => this.togglePersonHandler()}>Toggle Switch</button>
             {persons}
         </div>
-    </StyleRoot>
+
   );
   }
 }
-export default Radium(App);
+export default App;
